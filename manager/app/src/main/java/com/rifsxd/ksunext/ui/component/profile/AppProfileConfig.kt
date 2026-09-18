6badaddeca5943b3433556c034ab522dd5089886
thorgamechanger-cloud <thorgamechanger@gmail.com>
2026-09-19 01:23:07 +0530

    Update AppProfileConfig.kt

--- manager/app/src/main/java/com/rifsxd/ksunext/ui/component/profile/AppProfileConfig.kt
+++ manager/app/src/main/java/com/rifsxd/ksunext/ui/component/profile/AppProfileConfig.kt
@@ -1,4 +1,4 @@
-package com.rifsxd.ksunext.ui.component.profile
+package com.ayan.thor.ui.component.profile
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.OutlinedTextField
