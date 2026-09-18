26f6870babb375fddff9fbdae826481478d8fddc
thorgamechanger-cloud <thorgamechanger@gmail.com>
2026-09-19 01:24:34 +0530

    Update TemplateConfig.kt

--- manager/app/src/main/java/com/rifsxd/ksunext/ui/component/profile/TemplateConfig.kt
+++ manager/app/src/main/java/com/rifsxd/ksunext/ui/component/profile/TemplateConfig.kt
@@ -1,4 +1,4 @@
-package com.rifsxd.ksunext.ui.component.profile
+package com.ayan.thor.ui.component.profile
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
